ee05a3c4577d437272e084ffb5f93363194022ab
jojapoppa <jojapoppa@fedoragold.com>
2022-05-04 05:28:25 +0200

    build test

--- src/wallet/wallet2.h
+++ src/wallet/wallet2.h
@@ -34,7 +34,7 @@
 
 #include <boost/program_options/options_description.hpp>
 #include <boost/program_options/variables_map.hpp>
-//#if BOOST_VERSION >= 107400
+// //#if BOOST_VERSION >= 107400
 #include <boost/serialization/library_version_type.hpp>
 //#endif
 #include <boost/serialization/list.hpp>
